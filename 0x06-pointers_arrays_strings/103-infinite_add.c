0bb139a01ac96e551c0a47d472d6b317177b06e0
ngush67 <olsonngula@gmail.com>
2023-03-29 23:24:02 -0700

    Now?

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 /**
-  * infinite_add - adds two numbers
-  * @n1: number1
-  * @n2: number2
-  * @r: result
-  * @size_r: size result
-  * Return: r addition
-**/
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, a, b, c, n, aux, dec = 0;
+	int index, n1_len = 0, n2_len = 0;
 
-	while (n1[i] != '\0')
-	{
-	i++;
-	}
-	while (n2[j] != '\0')
-	{
-	j++;
-	}
-	for (n = 0; n < j || n < i; n++)
-	{
-	a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
-	b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
-	c = a + b + dec;
-	r[n] = (c % 10) + '0';
-	dec = c > 9 ? 1 : 0;
-	}
-	if (dec == 1)
-	{ r[n] = '1';
-	r[n + 1] = '\0'; }
-	else
-	{ r[n] = '\0';
-	n--; }
-	for (i = 0; i < n + 1; i++)
-	{
-	for (j = 0; j < (n - i); j++)
-	{
-	aux = r[j + 1];
-	r[j + 1] = r[j];
-	r[j] = aux;
-	}
-	}
-return (n < size_r - 1 ? r : 0);
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
