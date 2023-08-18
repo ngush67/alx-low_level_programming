5476e32c8fe1953a49363e00c98d6d9c8acdf717
Ngush <olsonngula@gmail.com>
2023-08-18 03:27:14 -0700

    Fixed

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 /**
- *  * free_dlistint - frees a doubly linked list with only int data, no strings
- *   * @head: pointer to head of list
- *    */
+ * free_dlistint - frees a doubly linked list with only int data, no strings
+ * @head: pointer to head of list
+ */
 void free_dlistint(dlistint_t *head)
 {
-		/* return if empty list */
-		if (!head)
-					return;
+	/* return if empty list */
+	if (!head)
+		return;
 
-			while (head && head->next)
-					{
-								head = head->next;
-										free(head->prev);
-											}
-				free(head);
+	while (head && head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
 }
